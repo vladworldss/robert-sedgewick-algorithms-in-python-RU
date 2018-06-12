d7fb6aa3e8ab3c3421f2b0e5272e0fe8e99eb49f
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-06-12 16:02:34 +0300

    ex3.6 - динамический массив для решето эратосфена

---
+++ Chapter03/ex3.6/main.c
@@ -0,0 +1,52 @@
+//
+// Created by Workstation on 10.06.2018.
+//
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv){
+
+    // =======================
+    printf("\n*********\n");
+    printf("Example 3.6:\n");
+    printf("*********\n");
+
+    const int SIEVE_SIZE = atoi(argv[1]);
+    size_t INT_SIZE = sizeof(int);
+    int* array = calloc(SIEVE_SIZE*INT_SIZE, INT_SIZE);
+    if (array == NULL){
+        perror("Insufficient memory");
+        return -1;
+    }
+
+    int ar_idx =0;
+    for(ar_idx = 2; ar_idx < SIEVE_SIZE; ar_idx++){
+        array[ar_idx] = 1;
+    }
+
+    for(ar_idx = 2; ar_idx < SIEVE_SIZE; ar_idx++){
+        if (array[ar_idx]){
+            for (int inner_idx = ar_idx; inner_idx*ar_idx < SIEVE_SIZE; inner_idx++){
+                array[inner_idx*ar_idx] = 0;
+            }
+        }
+    }
+
+    printf("FIRST %d SIMPLE VALUES\n\n", SIEVE_SIZE);
+    for (int printed = 0, idx = 0; idx < SIEVE_SIZE; idx++){
+        if (array[idx] == 1){
+            printf("%d ", idx);
+            printed++;
+        }
+
+        if (printed > 0 & ((printed % 10) == 0)){
+            printf("\r\n");
+            printed = 0;
+        }
+    }
+
+    return 0;
+}
