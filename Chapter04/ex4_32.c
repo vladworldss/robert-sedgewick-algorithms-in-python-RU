be30e42169ae41cb1df29a36d7a8d50110d0eece
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-08-12 20:18:46 +0100

    ex4_32 - проверка на переполнение и пустоту для array_queue. Эвристика для разряженной очереди. в unsafe_array_queue.c сохранена исходная учебная версия.

--- Chapter04/ex4_32.c
+++ Chapter04/ex4_32.c
@@ -1,16 +1,25 @@
+/* ex4_32.c
+ *
+ * Модифицируйте приведенную в тексте реализацию очереди FIFO 
+ * на базе массива (array_queue) так, чтобы она вызывала функцию
+ * QueurError(), если программа-клиент пытается выполнить операцию get,
+ * когда очередь пуста, или операцию put, когда очередь переполнена.
+ **/
+
 #include <stdio.h>
 #include <string.h>
 
 #include "Queue.h"
 
-void test_list(){
-	printf("test list\n");
+void test_get(){
+	printf("test get\n");
 	queue_init(2);
 	printf("%c\n", queue_get());
 }
 
-void test_array(){
-	printf("test array\n");
+
+void test_put(){
+	printf("test put\n");
 
 	int N = 3;
 	queue_init(N);
@@ -24,10 +33,43 @@ void test_array(){
 	}
 }
 
+
+void test_normal(){
+	printf("test normal\n");
+	int N = 10;
+	queue_init(N);
+
+	for (int i=0; i < 2; i++){
+		printf("putting %d into queue\n", i);
+		queue_put(i);
+	}
+	int res;
+	for (int i=0; i < 2; i++){
+		res = queue_get();
+		printf("getting %d from queue\n", res);
+	}
+
+	for (int i=0; i < 2; i++){
+		printf("putting %d into queue\n", i);
+		queue_put(i);
+	}
+
+	for (int i=0; i < 2; i++){
+		res = queue_get();
+		printf("getting %d from queue\n", res);
+	}
+}
+
+
 int main(int argc, char **argv){
-	if (!strcmp(argv[1], "l"))
-		test_list();
-	else if (!strcmp(argv[1], "a"))
-		test_array();
+
+	if (!strcmp(argv[1], "g"))
+		test_get();
+	else if (!strcmp(argv[1], "p"))
+		test_put();
+	else if (!strcmp(argv[1], "n"))
+		test_normal();
+	else
+		printf("Choise from ['g', 'p', 'n']\n");
 	return 0;
 }
--- Chapter04/unsafe_array_queue.c
+++ Chapter04/unsafe_array_queue.c
@@ -1,5 +1,7 @@
-/* array_queue.c
- *
+/* unsafe_array_queue.c
+ * 
+ * НЕБЕЗОПАСНАЯ ВЕРСИЯ ОЧЕРЕДИ (отсутствуют проверки на пустоту и переполнение)!
+ * 
  * К содержимому очереди относятся все элементы массива, расположенные
  * между индексами head и tail; при этом учитывается переход с конца
  * массива на элемент с индексом 0 в начале массива.
@@ -19,57 +21,25 @@
 static Item* q;
 static int N, head, tail;
 
-void QueueError(char* msg){
-    perror(msg);
-    exit(1);
-}
+
 
 void queue_init(int maxN){
 	N = maxN+1;
     q = calloc(N, sizeof(Item));
-    head = 0;
+    head = N;
     tail = 0;
 }
 
 int queue_empty(){
-    return head == tail;
+    return head % N == tail;
 }
 
 void queue_put(Item item){
-	if (tail > N){
-		errno = EAGAIN;
-		QueueError("Queue is full");
-	}
-
     q[tail++] = item;
+    tail = tail % N;
 }
 
 Item queue_get(){
-	if ((head == tail) && (head == 0)){
-		errno = EAGAIN;
-		QueueError("Queue is empty");
-	}
-
-	Item res = q[head++];
-	int count = N - head;
-
-	/* если очередь в начале разряжена больше, чем наполовину,
-	 * то копируем срез. 
-	 * */
-	if (head > count){
-		int i = head, j = 0;
-
-		while (i <= tail){
-			q[j++] = q[i++];
-		}
-		// Обновляем курсоры
-		head = 0;
-		tail = j;
-	}
-	else{
-		return res;	
-	}
-    
+    head = head % N;
+    return q[head++];
 }
-
-// вернуть методы unsafe для сравнения
